68c824ea624bf9adfd712ceaadb64f11578e069e
MpiloNM95 <mpilonmafu95@gmail.com>
2022-11-08 12:39:31 +0200

    Initial Commit

---
+++ Splitthecheque/SplitthechequeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitthechequeApp.swift
+//  Splitthecheque
+//
+//  Created by Mpilo Mafu on 2022/11/08.
+//
+
+import SwiftUI
+
+@main
+struct SplitthechequeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
